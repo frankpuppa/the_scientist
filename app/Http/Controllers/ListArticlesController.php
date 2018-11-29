88d1111ccac49559819cb83ace8b1b06fb0a5bfe
Francesco Della Puppa <zorzf@vdagroup.com>
2018-11-29 14:46:27 +0100

    add controller

---
+++ app/Http/Controllers/ListArticlesController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListArticlesController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return response()->json([]);
+    }
+}
